53bd15199c60dfdff0aa6d53a50a3565f54251e9
abhishek-166 <62430830+abhishek-166@users.noreply.github.com>
2020-05-08 14:39:57 +0530

    Add files via upload

---
+++ balloon.js
@@ -0,0 +1,45 @@
+window.onload=function(){
+    var canvas=document.getElementById("circle");
+    var context=canvas.getContext("2d");
+    context.arc(200,470,30,0,2*Math.PI,false);
+    
+    context.fillStyle="#FFFF00";
+    context.fill();
+    context.lineWidth=5;
+    context.strokeStyle="black";
+    context.stroke();
+    drawLevel();
+    
+};
+var x=50;
+var y=500;
+var speed=10;
+var speedY=5;
+
+
+var update= function(){
+
+while(y>=0){
+
+var speedY=1;
+y+=speedY
+};
+}
+
+var draw=function(){
+    
+    ctx.fillStyle="#fff000";
+    ctx.fill();
+    context.beginPath();
+      context.clearRect(x - 29, y - 29, 62,62);
+
+context.closePath();
+
+}
+var step=function(){
+    update();
+    draw();
+    window.requestAnimationFrame(step);
+}
+  step();
+
